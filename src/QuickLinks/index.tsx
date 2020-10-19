6b0f33d1bf7f606771e35bb1a5f99220b3ba81a9
Tom Mitchelmore <tommitchelmore@outlook.com>
2020-10-19 19:10:24 +0100

    Init and README

---
+++ src/QuickLinks/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, Page } from '@geist-ui/react'
+
+export default function index() {
+    return (
+        <Page dotBackdrop>
+            
+        </Page>
+    )
+}
